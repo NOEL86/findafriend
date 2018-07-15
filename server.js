397fef44453bd0db967520c178965a74fe50a710
Ashley <anoelj86@yahoo.com>
2018-07-15 12:17:45 -0600

    friends list added

--- server.js
+++ server.js
@@ -18,25 +18,84 @@ app.get('/app/public/survey', function (req, res) {
 
 var friends = [
     {
-        routeName: "yoda",
-        name: "Yoda",
-        Job: "Jedi Master",
-        age: 900,
-        forcePoints: 2000
+        "name": "Keith Urban",
+        "photo": "http://www.soundslikenashville.com/wp-content/uploads/2017/01/Keith-Urban-1484073384.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
     },
     {
-        routeName: "darthmaul",
-        name: "Darth Maul",
-        Job: "Sith Lord",
-        age: 200,
-        forcePoints: 1200
+        "name": "Barack Obama",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/8/8d/President_Barack_Obama.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
     },
     {
-        routeName: "obiwankenobi",
-        name: "Obi Wan Kenobi",
-        Job: "Jedi Master",
-        age: 55,
-        forcePoints: 1350
+        "name": "Ellen DeGeneres",
+        "photo": "https://cdn1.thr.com/sites/default/files/imagecache/NFE_portrait/2017/03/ellen_headshot_8.26-p_2017.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Reese Witherspoon",
+        "photo": "https://www.workingmother.com/sites/workingmother.com/files/styles/1000_1x_/public/images/2017/02/reese-witherspoon-600.jpg?itok=IlsPcIGP&fc=50,50",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Taylor Swift",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Taylor_Swift_Red_Tour_5%2C_2013.jpg/220px-Taylor_Swift_Red_Tour_5%2C_2013.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
     }
 ];
 
