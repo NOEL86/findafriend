fe0a8e985fc9d77afe35203d5edc36eebf82a909
Ashley <anoelj86@yahoo.com>
2018-07-15 12:07:44 -0600

    server up

--- server.js
+++ server.js
@@ -1,5 +1,47 @@
 var express = require("express");
-//requires body-parser
-//requires path
+var bodyParser = require("body-parser");
+var path = require("path");
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/app/public/home', function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+})
+app.get('/app/public/survey', function (req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+})
 
 
+var friends = [
+    {
+        routeName: "yoda",
+        name: "Yoda",
+        Job: "Jedi Master",
+        age: 900,
+        forcePoints: 2000
+    },
+    {
+        routeName: "darthmaul",
+        name: "Darth Maul",
+        Job: "Sith Lord",
+        age: 200,
+        forcePoints: 1200
+    },
+    {
+        routeName: "obiwankenobi",
+        name: "Obi Wan Kenobi",
+        Job: "Jedi Master",
+        age: 55,
+        forcePoints: 1350
+    }
+];
+
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
