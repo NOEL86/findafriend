076fec2bb50b49406e65c768e0dfbb43bbb1e6b5
Ashley <anoelj86@yahoo.com>
2018-07-19 16:46:06 -0600

    should work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,8 +8,45 @@ module.exports = function (app) {
     })
 
     app.post('/api/friends', function (req, res) {
-        friendsData.push(res.body);
-        res.json(true);
+        console.log(req, body);
+
+        //this is a placeholder object to store objects while you run your
+        //calculation to determine best matches.
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: Infinity
+        };
+
+        var userData = req.body;
+        var userScores = userData.scores;
+
+        var totalDifference;
+
+        for (var i = 0; i < friendsData.length; i++) {
+            var currentFriend = friendsData[i];
+            totalDifference = 0;
+
+            console.log(currentFriend.name);
+
+            for (var j = 0; j < currentFriend.scores.length; j++) {
+                var currentFriendScore = currentFriend.scores[j];
+                var currentUserScore = userScores[j];
+
+                //returns the absolute value of the currentUserScore - the currentFriendScore
+                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+            }
+
+            if (totalDifference <= bestMatch.friendDifference) {
+                bestMatch.name = currentFriend.name;
+                bestMatch.photo = currentFriend.photo;
+                bestMatch.friendDifference = totalDifference;
+            }
+        }
+
+        friendsData.push(userData);
+        console.log(bestMatch);
+        res.json(bestMatch);
     })
 
 }
