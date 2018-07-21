72d34332c72393fb1545eb575bf876cb3fc32dd4
Ashley <anoelj86@yahoo.com>
2018-07-20 21:38:24 -0600

    modal not displaying data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,45 +8,58 @@ module.exports = function (app) {
     })
 
     app.post('/api/friends', function (req, res) {
-        console.log(req, body);
 
-        //this is a placeholder object to store objects while you run your
-        //calculation to determine best matches.
+
+        // We will use this object to hold the "best match". We will constantly update it as we
+        // loop through all of the options
+        // console.log(req.body);
         var bestMatch = {
             name: "",
             photo: "",
             friendDifference: Infinity
         };
 
+        // Here we take the result of the user"s survey POST and parse it.
         var userData = req.body;
         var userScores = userData.scores;
 
+        // This variable will calculate the difference between the user"s scores and the scores of
+        // each user in the database
         var totalDifference;
 
+        // Here we loop through all database.
         for (var i = 0; i < friendsData.length; i++) {
             var currentFriend = friendsData[i];
             totalDifference = 0;
 
             console.log(currentFriend.name);
 
+            // We then loop through all the scores of each friend in database
             for (var j = 0; j < currentFriend.scores.length; j++) {
                 var currentFriendScore = currentFriend.scores[j];
                 var currentUserScore = userScores[j];
 
-                //returns the absolute value of the currentUserScore - the currentFriendScore
+                // We calculate the difference between the scores and sum them into the totalDifference
                 totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
             }
 
+            // If the sum of differences is less then the differences of the current "best match"
             if (totalDifference <= bestMatch.friendDifference) {
+                // Reset the bestMatch to be the new friend.
                 bestMatch.name = currentFriend.name;
                 bestMatch.photo = currentFriend.photo;
                 bestMatch.friendDifference = totalDifference;
             }
         }
 
+
         friendsData.push(userData);
-        console.log(bestMatch);
+
+        // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
+        console.log(bestMatch.name + " I'm on the back end");
+
         res.json(bestMatch);
     })
+
 
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,6 +4,9 @@ module.exports = function (app) {
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, "/../public/home.html"));
     })
+    app.get('/api/friends', function (req, res) {
+        res.sendFile(path.join(__dirname, "/api/friends.js"));
+    })
     app.get('/survey', function (req, res) {
         res.sendFile(path.join(__dirname, "/../public/survey.html"));
     })
